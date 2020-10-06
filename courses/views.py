ed23d8d14860d18bbb7dc3c03c3e80adc0e3dbcc
al <alxayeed@gmail.com>
2020-10-06 16:48:10 +0600

    content and module reordering feature addedf in the template, using jquery dra-and-drop

--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 
 # class ManageCourseListView(ListView):
@@ -169,3 +170,27 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    """
+    Reorder a module using ajax
+    """
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    """
+    Reorder a content using ajax
+    """
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
