ff89cb5303f887d6126bd2bb122e85e79292096f
al <alxayeed@gmail.com>
2020-10-09 12:13:02 +0600

    per-view caching added to StudentCourseDetailView

--- students/urls.py
+++ students/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('register/',
@@ -11,9 +12,9 @@ urlpatterns = [
     path('courses/',
          views.StudentCourseListView.as_view(),
          name='student_course_list'),
-    path('course/<pk>/', views.StudentCourseDetailView.as_view(),
+    path('course/<pk>/', cache_page(60*15)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail'),
-    path('course/<pk>/<module_id>/', views.StudentCourseDetailView.as_view(),
+    path('course/<pk>/<module_id>/', cache_page(60*15)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail_module')
 
 ]
