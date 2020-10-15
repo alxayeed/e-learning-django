7b8a645ffcf17132e0878be1395b7e33c2649d88
al <alxayeed@gmail.com>
2020-10-15 09:23:37 +0600

    chat room template updated to show user and time

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,10 +1,12 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = 'chat_%s' % self.id
         # join the group
@@ -26,6 +28,7 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
 
         # send message to WebSocket
         # self.send(text_data=json.dumps({'message': message}))
@@ -35,7 +38,9 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
             }
         )
 
